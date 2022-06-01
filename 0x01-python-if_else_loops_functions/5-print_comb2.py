ca1532491de9ebb982b5916befc60ef9a9b263c9
root <root@Rashisky-Paper.localdomain>
2022-06-01 22:42:21 +0100

    Python if/else, loops, functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for x in range(1, 100):
+for x in range(0, 100):
     if x != 99:
-        print(f"{x:02}", end=", ")
+        print(f"{x:02d}", end=", ")
     else:
         print(x)
