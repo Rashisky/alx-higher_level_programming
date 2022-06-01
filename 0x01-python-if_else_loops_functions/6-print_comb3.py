5f4de2d4bd4a212db3ca6ea8c1026204eb98ff82
Rashisky <99407788+Rashisky@users.noreply.github.com>
2022-06-01 23:50:13 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(1, 99):
-    if x > 9:
+    if x > 9 and x != 99:
         for i in range(x):
             if f"{i:02d}" == str(x % 10) + str(x // 10):
                 x = i
@@ -8,4 +8,5 @@ for x in range(1, 99):
             print("{:02d}".format(x), end=" ")
     elif x <= 9:
         print("{:02d}".format(x), end=" ")
-print("\n")
+    else:
+        print("{:02d}".format(x))
