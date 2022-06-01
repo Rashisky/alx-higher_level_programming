602ca057d4dce8a715e2ca60f37bbe80f7e3b3dd
root <root@Rashisky-Paper.localdomain>
2022-06-01 22:40:38 +0100

    Python if/else, loops, functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    print(f"{x:02d}", end=", ")
-print(x)
+    if x != 99:
+        print(f"{x:02}", end=", ")
+    else:
+        print(x)
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,12 +2,10 @@
 for x in range(1, 100):
     if x > 9:
         for i in range(x):
-            c = str(x % 10) + str(x // 10)
-            if f"{i:02d}" == c:
-                break
+            if f"{i:02d}" == str(x % 10) + str(x // 10):
+                x = i
         if (x-1 == i):
             print(f"{x:02d}", end=" ")
     elif x <= 9:
         print(f"{x:02d}", end=" ")
-    x += 1
 print("\n")
