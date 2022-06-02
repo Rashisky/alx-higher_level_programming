1d1e41624ded1778880a7bf0f3c65b403422d950
root <root@Rashisky-Paper.localdomain>
2022-06-02 23:13:34 +0100

    python-import & modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,11 +4,7 @@ import calculator_1 as cal
 if __name__ == "__main__":
     a = 10
     b = 5
-    c = cal.add(a, b)
-    d = cal.sub(a, b)
-    e = cal.mul(a, b)
-    f = cal.div(a, b)
-    print("{} + {} = {}".format(a, b, c))
-    print("{} - {} = {}".format(a, b, c))
-    print("{} * {} = {}".format(a, b, c))
-    print("{} / {} = {}".format(a, b, c))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, add(a, b)))
+    print("{} * {} = {}".format(a, b, add(a, b)))
+    print("{} / {} = {}".format(a, b, add(a, b)))
