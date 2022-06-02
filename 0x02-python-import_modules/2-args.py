cde2003e124102d80573f90f04cf3943e344ae26
root <root@Rashisky-Paper.localdomain>
2022-06-02 23:28:04 +0100

    python-import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     length = len(sys.argv) - 1
     if length == 0:
         print(f"{length} arguments.")
+    elif length == 1:
+        print(f"{length} argument:")
+        print(f"{length}: {sys.argv[length]}")
     else:
         print(f"{length} arguments:")
         for i in range(1, length + 1):
