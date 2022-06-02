0259fbdc1e99b99ce4976a67a84c6648efff5b34
root <root@Rashisky-Paper.localdomain>
2022-06-02 23:01:15 +0100

    python-import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 if __name__ == "__main__":
-    c = add_0.add(a = 1, b = 2)
-    print(f"{a} + {b} = {c}")
+    a = 1
+    b = 2
+    c = add(a, b)
+    print(f"{a:d} + {b:d} = {c:d}")
