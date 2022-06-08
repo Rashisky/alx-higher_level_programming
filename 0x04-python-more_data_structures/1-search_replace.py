3ac345b4940307b07b3e2744c5b5d5cb5d48c34f
root <root@Rashisky-Paper.localdomain>
2022-06-09 00:10:07 +0100

    more on data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    new_list[new_list.index(search)] = replace
+    for i, v in enumerate(new_list):
+        if v == search:
+            new_list[i] = replace
     return (new_list)
