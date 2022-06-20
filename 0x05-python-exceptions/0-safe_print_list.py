3812f038ec66c60882b9db598d76744f2a0310c4
Rashisky <root@Rashisky-Paper.localdomain>
2022-06-20 22:43:41 +0100

    Exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(element, end="")
             count += 1
-        except Exception as e:
+        except IndexError as e:
             break
     print("")
     return (count)
