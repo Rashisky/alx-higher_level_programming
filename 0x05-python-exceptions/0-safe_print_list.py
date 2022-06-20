c2c714914e76a31a27146338e293cd80db9028f3
Rashisky <root@Rashisky-Paper.localdomain>
2022-06-20 22:41:56 +0100

    Exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     for element in my_list:
-        if (count < x):
-            try:
-                print(element, end="")
-                count += 1
-            except Exception as e:
-                pass
+        try:
+            print(element, end="")
+            count += 1
+        except Exception as e:
+            break
     print("")
     return (count)
