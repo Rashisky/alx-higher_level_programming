fd6dfad4800aed8b42d4bc5925608c2106f21ab5
Rashisky <root@Rashisky-Paper.localdomain>
2022-06-23 01:19:36 +0100

    Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,11 +7,9 @@ class Square:
     def __init__(self, size=0):
         """ Initialize Private Instance attribute """
 
-        """ checks if attribute is an integer """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
-        """ checks if attribute is negative """
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
