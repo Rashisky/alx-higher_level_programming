7c4ce476900204b369c8a4725517b6d7d4c77b8f
Rashisky <root@Rashisky-Paper.localdomain>
2022-07-05 01:05:43 +0100

    Inheritance in Python

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,6 +7,6 @@ class Square(Rectangle):
     """Find the square of the geometry"""
     def __init__(self, size):
         """Initialize the size of the geometry"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,6 +7,6 @@ class Square(Rectangle):
     """Describe a Square Geometry"""
     def __init__(self, size):
         """Initialize the value of the square"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
