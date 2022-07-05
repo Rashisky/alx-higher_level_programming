88050961cc83be3261436e1cb54125310ce885a1
Rashisky <root@Rashisky-Paper.localdomain>
2022-07-05 01:16:31 +0100

    Inheritance in Python

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,7 @@ returns False
 
 
 def is_same_class(obj, a_class):
-    """ Comparison function """
-    if isinstance(type(obj), a_class):
+    """ Comparison function"""
+    if type(obj) == a_class:
         return True
     return False
