34b1644529161a116012b2d1ac347315e2de2652
Rashisky <root@Rashisky-Paper.localdomain>
2022-07-05 23:57:56 +0100

    Python file handling

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,7 +3,7 @@
 import json
 
 
-def load_from_json(fileaname):
+def load_from_json(filename):
     """Create a Python object from a JSON file."""
     with open(filename) as f:
         return json.load(f)
